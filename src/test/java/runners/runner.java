455ff5e0360564e715e3855334e1c3a014076cf6
D-almasaud <you@example.com>
2024-03-25 13:01:56 +0300

    first commit

---
+++ src/test/java/runners/runner.java
@@ -0,0 +1,19 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        glue = "stepdefinitions",
+        features = "src/test/resources/features",
+        tags = "",
+        dryRun = false
+
+
+)
+
+public class runner {
+}
